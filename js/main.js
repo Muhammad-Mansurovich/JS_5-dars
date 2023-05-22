17459370b558a0d27be6954314fddbddb2f5e466
Muhammad Murtazokulov <133627275+Muhammad-Mansurovich@users.noreply.github.com>
2023-05-23 01:16:14 +0500

    Bu JS 5-dars
    yuklangan kodlar

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* JS types: undefined,boolean,object */
+
+/*let soz = "text";
+document.write(typeof soz);*/
+
+/*let rost = true;*/                /* true,false */
+/*let rost = 45==45?true:false;
+document.write(rost);*/
+
+/*let arrayObject = [45,78,'text','javascript','son',true,false];
+console.log(arrayObject[4]);*/
+/*let oddiyObject = {ism:"Oubek",familiya:"Otakulov",yoshi:15}; /!*key:value*!/
+console.log(oddiyObject.ism,oddiyObject.familiya);*/
+
+/* Math in JS */
+
+let round = Math.round(8.5);       /* yaxlitlash */
+let pow = Math.pow(4,4);          /* darajaga ko'tarish */
+let sqrt = Math.sqrt(16);           /* ildizini topish */
+let abs = Math.abs(-12.8);          /* absolyutlash */
+let ceil = Math.ceil(4.1);          /* yaxlitlash */
+let floor = Math.floor(4.9);        /* yaxlitlash */
+let trunc = Math.trunc(4.5);        /* yaxlitlash */
+let min = Math.min(12,1,200,399,123); /* minimal sonini topish */
+let max = Math.max(12,1,200,399,123);   /* maximal sonini topish */
+
+document.write(max);
+
+
